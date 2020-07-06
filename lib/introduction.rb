873098e71df374d777df05f6fae38dfa824d61d2
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-06 14:46:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name)
-  puts "Hi, my name is #{name} and I am learning to program in Ember.js."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
